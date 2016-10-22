d8fd62aefadb20eb160249783a48f9254ed96c0b
Jami <jamischwarzwalder@gmail.com>
2016-10-22 01:50:07 -0700

    Factory Patterns from in class examples

--- FactoryPatterns/src/edu/greenriver/it/life/LifeCards.java
+++ FactoryPatterns/src/edu/greenriver/it/life/LifeCards.java
@@ -6,11 +6,39 @@
  */
 package edu.greenriver.it.life;
 
+import java.util.List;
+
+import edu.greenriver.it.boardgame_parts.IGameCards;
+import edu.greenriver.it.game_component.GameCard;
+
 /**
  *
  * @author Jami Schwarzwalder
  * @version 1.1
  */
-public class LifeCards {
+public class LifeCards implements IGameCards{
+
+	/**
+	 * 
+	 * @param type
+	 * @return
+	 * @see edu.greenriver.it.boardgame_parts.IGameCards#getDeck(java.lang.String)
+	 */
+	@Override
+	public List<GameCard> getDeck(String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param cards
+	 * @see edu.greenriver.it.boardgame_parts.IGameCards#shuffle(java.util.List)
+	 */
+	@Override
+	public void shuffle(List<GameCard> cards) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- FactoryPatterns/src/edu/greenriver/it/monopoly/MonopolyBoard.java
+++ FactoryPatterns/src/edu/greenriver/it/monopoly/MonopolyBoard.java
@@ -6,11 +6,78 @@
  */
 package edu.greenriver.it.monopoly;
 
+import java.util.List;
+
+import edu.greenriver.it.abstract_factory.ArrayList;
+import edu.greenriver.it.boardgame_parts.IGameBoard;
+import edu.greenriver.it.game_component.BoardSpace;
+import edu.greenriver.it.game_component.GamePiece;
+
 /**
  *
  * @author Jami Schwarzwalder
  * @version 1.1
  */
-public class MonopolyBoard {
+public class MonopolyBoard implements IGameBoard {
+
+	BoardSpace go = new BoardSpace("Go");
+	BoardSpace mediterraenean = new BoardSpace("Medditerian");
+	BoardSpace baltic = new BoardSpace("baltic ave");
+	
+	spaces = new ArrayList<BoardSpace>();
+	
+	spaces.add(go);
+	spaces.add(mediterraenean);
+	spaces.add(baltic);
+	
+	/**
+	 * 
+	 * @return
+	 * @see edu.greenriver.it.boardgame_parts.IGameBoard#getBoardspaces()
+	 */
+	@Override
+	public List<BoardSpace> getBoardspaces() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param space
+	 * @param piece
+	 * @see edu.greenriver.it.boardgame_parts.IGameBoard#addPiece(edu.greenriver.it.game_component.BoardSpace, edu.greenriver.it.game_component.GamePiece)
+	 */
+	@Override
+	public void addPiece(BoardSpace space, GamePiece piece) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * 
+	 * @param space
+	 * @param piece
+	 * @return
+	 * @see edu.greenriver.it.boardgame_parts.IGameBoard#removePiece(edu.greenriver.it.game_component.BoardSpace, edu.greenriver.it.game_component.GamePiece)
+	 */
+	@Override
+	public boolean removePiece(BoardSpace space, GamePiece piece) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * 
+	 * @param source
+	 * @param destination
+	 * @param piece
+	 * @return
+	 * @see edu.greenriver.it.boardgame_parts.IGameBoard#move(edu.greenriver.it.game_component.BoardSpace, edu.greenriver.it.game_component.BoardSpace, edu.greenriver.it.game_component.GamePiece)
+	 */
+	@Override
+	public boolean move(BoardSpace source, BoardSpace destination, GamePiece piece) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
