c24b7a3050b23b6c5bcd8bf4d963fa90f6e51edb
Jami <jamischwarzwalder@gmail.com>
2016-11-03 00:29:24 -0700

    Pattern returns user's order

--- Schwarzwalder Abstract Factor Pattern Assignment/src/edu/greenriver/it/schwarzwalder/driver/Driver.java
+++ Schwarzwalder Abstract Factor Pattern Assignment/src/edu/greenriver/it/schwarzwalder/driver/Driver.java
@@ -2,9 +2,11 @@
  * Jami Schwarzwalder
  * Oct 29, 2016
  * Driver.java
- * [Description Here]
+ * Utilizing Factory Patterns with Console
  */
 package edu.greenriver.it.schwarzwalder.driver;
+
+import java.text.DecimalFormat;
 
 import edu.greenriver.it.schwarzwalder.factory.EUOrderFactory;
 import edu.greenriver.it.schwarzwalder.factory.IOrderFactory;
@@ -18,6 +20,7 @@ import edu.greenriver.it.schwarzwalder.shippingrates.Rate;
  */
 public abstract class Driver {
 
+	private final static int SUBTOTAL = 20;
 
 	/**
 	 * 
@@ -25,11 +28,18 @@ public abstract class Driver {
 	 */
 	public static void main(String[] args) {
 
+		createOrder();
+	}
+
+	/**
+	 * 
+	 */
+	private static void createOrder() {
 		Console.print("Where would you like to ship to?");
 		Console.print("1: US");
 		Console.print("2: EU");
 
-		IOrderFactory factory;
+		IOrderFactory factory = null;
 		int userRegion;
 		do {
 			userRegion = Console.getInt("Select a Region");
@@ -40,17 +50,52 @@ public abstract class Driver {
 				String region = Console.getString("Enter Country name or code");
 				factory = new EUOrderFactory(region);
 			}
-		} while (userRegion > 2 || userRegion < 1);
+		} while (factory == null);
 
 		String userOrderName = Console.getString("Enter order name");
 		double userWeight = Console.getDouble("Enter order weight");
-		
+
 		Rate shippingRate = null;
 		String userShipmentType;
-		do
-		{
+		do {
 			userShipmentType = Console.getString("Enter shipment type");
+			shippingRate = factory.getRateObject().getRate(userShipmentType, userWeight);
 		} while (shippingRate == null);
+
+		double tax = factory.getTaxObject().calculateTax(20);
+		double total = tax + SUBTOTAL + shippingRate.getRate();
+
+		DecimalFormat dollarFormat = new DecimalFormat("###0.00");
+
+		StringBuilder orderDetails = new StringBuilder();
+		orderDetails.append("Order details: ");
+		orderDetails.append(userOrderName);
+		orderDetails.append(" - weight: ");
+		orderDetails.append(userWeight);
+		orderDetails.append(" , shipping type: ");
+		orderDetails.append(userShipmentType);
+		orderDetails.append("\nShipping cost: ");
+		orderDetails.append(dollarFormat.format(shippingRate.getRate()));
+		orderDetails.append("\nSubtotal: ");
+		orderDetails.append(dollarFormat.format(SUBTOTAL));
+		orderDetails.append("\nTax: ");
+		orderDetails.append(dollarFormat.format(tax));
+		orderDetails.append("\nTotal: ");
+		orderDetails.append(dollarFormat.format(total));
+
+		Console.print(orderDetails.toString());
+
+		boolean validResponse = false;
+		do {
+			String placeNewOrder = Console.getString("Would you like to place another order? (Y/N)");
+
+			if (placeNewOrder.equalsIgnoreCase("yes") || placeNewOrder.equalsIgnoreCase("Y")) {
+				createOrder();
+				validResponse = true;
+			} else if (placeNewOrder.equalsIgnoreCase("no") || placeNewOrder.equalsIgnoreCase("N")) {
+				validResponse = true;
+			}
+		} while (!validResponse);
 
 	}
 
