d1587374543b35a1165036f4823631b51c2c60ee
Jami <jamischwarzwalder@gmail.com>
2016-11-15 11:36:39 -0800

    adding extra views

---
+++ FatFree/controllers/generateviews.php
@@ -0,0 +1,17 @@
+<?php
+	class GenerateViews {
+		public function showHome() {
+			echo 'Home Page';
+		}
+		
+		public function showAboutUs() {
+			echo 'About us page';
+			
+		}
+		
+		public function showSiteMap() {
+			echo 'Site Map page';
+			
+		}
+	}
+?>
--- FatFree/index.php
+++ FatFree/index.php
@@ -1,13 +1,33 @@
 <?php
-
+	//load autoloader
+	require('vendor/autoload.php');
+	
 	//get our router
 	$f3 = require('vendor/bcosca/fatfree-core/base.php');
 	
 	//create a few routes
-	$f3->route('GET /', function(){
-		echo 'hello world';
+	$f3->route('GET /', 'GenerateViews->showHome');//base route is 'GET /'
+	
+	$f3->route('GET /about', 'GenerateViews->showAboutUs');
+	
+	$f3->route('GET /site', 'GenerateViews->showSiteMap');
+	$f3->route('GET /calculatePrime/@number', function($f3, $params){
+		$number = $params['number'];
 		
-	} );//base route is 'GET /'
+		if($number <= 1){
+			//redirect
+			
+			//or http error code
+			$f3 -> error(404);
+		}
+	}
+	
+	//turn on more verbose error handling through router
+	$f3 ->set('DEBUG', 3); //0 means surpressed, 3 verbose
+	$f3 ->set('ONERROR', function($f3){
+		echo 'Error text '.$f3->get('ERROR.text').'<br>';
+		echo 'Error trace '.$f3->get('ERROR.trace').'<br>';
+	});
 	
 	//start router
 	$f3->run();
