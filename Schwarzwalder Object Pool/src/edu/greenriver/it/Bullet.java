e865431222096cf61cf94452c0187f850a7952cb
Jami <jamischwarzwalder@gmail.com>
2016-11-25 22:56:35 -0800

    working on making an Object Pool

---
+++ Schwarzwalder Object Pool/src/edu/greenriver/it/Bullet.java
@@ -0,0 +1,46 @@
+package edu.greenriver.it;
+import java.awt.Image;
+import java.awt.geom.Point2D;
+
+/**
+ * Jami Schwarzwalder
+ * Nov 25, 2016
+ * Bullet.java
+ * Example of Object you would want to pool
+ */
+
+/**
+ *
+ * @author Jami Schwarzwalder
+ * @version 1.1
+ */
+public class Bullet {
+	
+	private int velocity;
+	private Point2D postion;
+	private int lifeSpan;
+	private int damage;
+	private Image bulletImage;
+	
+	/**
+	 * Creates a new Bullet
+	 *
+	 * @param velocity
+	 * @param postion
+	 * @param lifeSpan
+	 * @param damage
+	 * @param bulletImage
+	 */
+	public Bullet(int velocity, Point2D postion, int lifeSpan, int damage, Image bulletImage) {
+		
+		this.velocity = velocity;
+		this.postion = postion;
+		this.lifeSpan = lifeSpan;
+		this.damage = damage;
+	}
+	
+	
+	
+	
+
+}
---
+++ Schwarzwalder Object Pool/src/edu/greenriver/it/ObjectPool.java
@@ -0,0 +1,16 @@
+/**
+ * Jami Schwarzwalder
+ * Nov 25, 2016
+ * ObjectPool.java
+ * [Description Here]
+ */
+package edu.greenriver.it;
+
+/**
+ *
+ * @author Jami Schwarzwalder
+ * @version 1.1
+ */
+public class ObjectPool {
+
+}
